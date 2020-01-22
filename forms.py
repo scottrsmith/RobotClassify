247ecb051857783d1e1a07094c7fde55c5d35181
Scott <scott@scottrobertsmith.com>
2020-01-22 12:21:27 -0800

    Added ML Lib

--- forms.py
+++ forms.py
@@ -1,250 +1,75 @@
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
-from wtforms.validators import DataRequired, AnyOf, URL, Optional
+from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField, IntegerField, FloatField
+from wtforms import SubmitField
+from wtforms.validators import DataRequired, AnyOf, URL, Optional, NumberRange
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from flask_uploads import UploadSet, DATA
+import config
 
-class ShowForm(FlaskForm):
-    artist_id = StringField(
-        'artist_id'
-    )
-    venue_id = StringField(
-        'venue_id'
-    )
-    start_time = DateTimeField(
-        'start_time',
-        validators=[DataRequired()],
-        default= datetime.today()
-    )
-    
-
-class VenueForm(FlaskForm):
+class ProjectForm(FlaskForm):
     name = StringField(
-        'name', validators=[DataRequired(message='Venue Name Required')]
-    )
-    city = StringField(
-        'city', validators=[DataRequired(message='City Name Required')]
-    )
-    state = SelectField(
-        'state', validators=[DataRequired(message='State Required')],
-        choices=[
-            ('AL', 'AL'),
-            ('AK', 'AK'),
-            ('AZ', 'AZ'),
-            ('AR', 'AR'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DE', 'DE'),
-            ('DC', 'DC'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('IA', 'IA'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('ME', 'ME'),
-            ('MT', 'MT'),
-            ('NE', 'NE'),
-            ('NV', 'NV'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NY', 'NY'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('MD', 'MD'),
-            ('MA', 'MA'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MS', 'MS'),
-            ('MO', 'MO'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VT', 'VT'),
-            ('VA', 'VA'),
-            ('WA', 'WA'),
-            ('WV', 'WV'),
-            ('WI', 'WI'),
-            ('WY', 'WY'),
-        ]
-    )
-    address = StringField(
-        'address', validators=[DataRequired(message='Address is Required')]
-    )
-    phone = StringField(
-        'phone', validators=[DataRequired(message='Phone is Required')]
-    )
+        'name', validators=[DataRequired(message='Project Name Required')])
 
-    genres = SelectMultipleField(
-        'genres', validators=[DataRequired()],
-        choices=[
-            ('Alternative', 'Alternative'),
-            ('Blues', 'Blues'),
-            ('Classical', 'Classical'),
-            ('Country', 'Country'),
-            ('Electronic', 'Electronic'),
-            ('Folk', 'Folk'),
-            ('Funk', 'Funk'),
-            ('Hip-Hop', 'Hip-Hop'),
-            ('Heavy Metal', 'Heavy Metal'),
-            ('Instrumental', 'Instrumental'),
-            ('Jazz', 'Jazz'),
-            ('Musical Theatre', 'Musical Theatre'),
-            ('Pop', 'Pop'),
-            ('Punk', 'Punk'),
-            ('R&B', 'R&B'),
-            ('Reggae', 'Reggae'),
-            ('Rock n Roll', 'Rock n Roll'),
-            ('Soul', 'Soul'),
-            ('Other', 'Other'),
-        ]
-    )
+    description = StringField(
+        'description', validators=[DataRequired(message='Description Required')])
+
+
+    trainingFile = FileField(validators=[FileRequired(),
+                                        FileAllowed(config.dataFiles, 
+                                        'csv files only')])
+
+    testingFile = FileField(validators=[FileRequired(),
+                                        FileAllowed(config.dataFiles, 
+                                        'csv files only')])
+
+class ProjectFormEdit(FlaskForm):
+    name = StringField(
+        'name', validators=[DataRequired(message='Project Name Required')])
+
+    description = StringField(
+        'description', validators=[DataRequired(message='Description Required')])
+    
+    #tainingFile = StringField()
+
+    #testingFile = StringField()
+
+    trainingFile = FileField(validators=[FileRequired(),
+                                        FileAllowed(config.dataFiles, 
+                                        'csv files only')])
+
+    testingFile = FileField(validators=[FileRequired(),
+                                        FileAllowed(config.dataFiles, 
+                                        'csv files only')])
+
   
 
-    website = StringField(
-        'website', validators=[Optional(), URL()]
-    )
-
-    seeking_talent = BooleanField(
-        'seeking_talent', default=False
-    )
-
-    seeking_description = StringField(
-        'seeking_description', validators=[Optional()]
-    )
-
-    image_link = StringField(
-        'image_link', validators=[DataRequired(), URL()]
-    )
-
-    facebook_link = StringField(
-        'facebook_link', validators=[Optional(), URL()]
-    )
-    
-    
-
-
-class ArtistForm(FlaskForm):
+class RunForm(FlaskForm):
     name = StringField(
-        'name', validators=[DataRequired()]
+        'name', validators=[DataRequired(message='Run Name Required')]
     )
-    city = StringField(
-        'city', validators=[DataRequired()]
+    description = StringField(
+        'description', validators=[DataRequired(message='Description Required')]
     )
-    state = SelectField(
-        'state', validators=[DataRequired()],
-        choices=[
-            ('AL', 'AL'),
-            ('AK', 'AK'),
-            ('AZ', 'AZ'),
-            ('AR', 'AR'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DE', 'DE'),
-            ('DC', 'DC'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('IA', 'IA'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('ME', 'ME'),
-            ('MT', 'MT'),
-            ('NE', 'NE'),
-            ('NV', 'NV'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NY', 'NY'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('MD', 'MD'),
-            ('MA', 'MA'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MS', 'MS'),
-            ('MO', 'MO'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VT', 'VT'),
-            ('VA', 'VA'),
-            ('WA', 'WA'),
-            ('WV', 'WV'),
-            ('WI', 'WI'),
-            ('WY', 'WY'),
-        ]
+    targetVariable = SelectField(
+        'state', validators=[DataRequired(message='Target Variable Required')],
+        choices=[]
     )
-    phone = StringField(
-        'phone'
+    key = SelectField(
+        'key', validators=[DataRequired('The Primary key is required')], choices=[]
     )
+    predictSetOut = SelectMultipleField(
+        'predictSetOut', validators=[DataRequired('The Predict requires at least two choices')], choices=[]
+    )
+   
+    scoring = SelectField('scoring', 
+                    validators=[DataRequired('A target scoring method is required')], 
+                    choices=[('f1','f1'),('r2','r2'),('Recall','Recall'),('Precision','Precision'),
+                             ('Accuracy','Accuracy')])
+    # Booleans
     
-    genres = SelectMultipleField(
-        'genres', validators=[DataRequired()],
-        choices=[
-            ('Alternative', 'Alternative'),
-            ('Blues', 'Blues'),
-            ('Classical', 'Classical'),
-            ('Country', 'Country'),
-            ('Electronic', 'Electronic'),
-            ('Folk', 'Folk'),
-            ('Funk', 'Funk'),
-            ('Hip-Hop', 'Hip-Hop'),
-            ('Heavy Metal', 'Heavy Metal'),
-            ('Instrumental', 'Instrumental'),
-            ('Jazz', 'Jazz'),
-            ('Musical Theatre', 'Musical Theatre'),
-            ('Pop', 'Pop'),
-            ('Punk', 'Punk'),
-            ('R&B', 'R&B'),
-            ('Reggae', 'Reggae'),
-            ('Rock n Roll', 'Rock n Roll'),
-            ('Soul', 'Soul'),
-            ('Other', 'Other'),
-        ]
-    )
+    basicAutoMethod = BooleanField('basicAutoMethod', default=False)
+
+    setProjectGoals = FloatField('setProjectGoals', validators=[DataRequired('A scoring goal is required'),
+                            NumberRange(min=0.0, max=1.0, message="A number between 0 and 1 is required")])
     
-    website = StringField(
-        'website', default=None, validators=[Optional(), URL()]
-    )
-
-    seeking_venue = BooleanField(
-        'seeking_venue', default=False
-    )
-
-    seeking_description = StringField(
-        'seeking_description', validators=[Optional()]
-    )
-
-    image_link = StringField(
-        'image_link', validators=[DataRequired(), URL()]
-    )
-
-
-    facebook_link = StringField(
-        'facebook_link', validators=[Optional(), URL()]
-    )
